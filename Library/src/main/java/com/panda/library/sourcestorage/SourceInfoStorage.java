4e131baacfe65fd7960fe2c98e1dbf9aa3301b8a
panpan <1720801477@qq.com>
2022-04-09 17:05:48 +0800

    first commit

---
+++ Library/src/main/java/com/panda/library/sourcestorage/SourceInfoStorage.java
@@ -0,0 +1,17 @@
+package com.panda.library.sourcestorage;
+
+import com.panda.library.SourceInfo;
+
+/**
+ * Storage for {@link SourceInfo}.
+ *
+ * @author Alexey Danilov (danikula@gmail.com).
+ */
+public interface SourceInfoStorage {
+
+    SourceInfo get(String url);
+
+    void put(String url, SourceInfo sourceInfo);
+
+    void release();
+}
